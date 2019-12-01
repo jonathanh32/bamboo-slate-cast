1a264cf4b4686c0a1baa44c528bd114cb5a6b3bc
Jonathan Hughes <jonathanbhughes@gmail.com>
2019-12-01 17:22:20 +0000

    docs: adding comments to classes

--- bamboo-slate-device.js
+++ bamboo-slate-device.js
@@ -18,7 +18,7 @@ export class BambooSlateDevice {
         this.tx = null;
         this.rx = null;
         this.uuid = [];
-        
+
         this.onDisconnected = this.onDisconnected.bind(this);
         this.onConnected = Function.prototype;
         this.onError = this.onError.bind(this);
@@ -26,6 +26,9 @@ export class BambooSlateDevice {
         this.onDraw = Function.prototype;
     }
 
+    /**
+     * Open the in-browser Bluetooth device selector. Must be called as part of user interaction (e.g. double-click).
+     */
     selectDevice() {
         var bamboo = this;
 
@@ -34,28 +37,31 @@ export class BambooSlateDevice {
                 "name": "Bamboo Slate"
             }],
             "optionalServices": [NORDIC_UART_SERVICE_UUID, WACOM_LIVE_SERVICE_UUID]
-        }).then(function(device){
+        }).then(function (device) {
             bamboo.device = device;
             bamboo.connect();
         });
     }
 
-    connect(device) {
+    /**
+     * Connect to the Bamboo Slate device.
+     */
+    connect() {
         var bamboo = this;
 
         // Listen for disconnect and start GATT connection
 
         this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
-        
+
         return this.device.gatt.connect().then(function (server) {
 
             bamboo.server = server;
 
             // Get UART service
             return server.getPrimaryService(NORDIC_UART_SERVICE_UUID);
-            
+
         }).then(function (service) {
-            
+
             // Get RX characteristic
             bamboo.uart = service;
             return bamboo.uart.getCharacteristic(NORDIC_UART_CHRC_RX_UUID);
@@ -81,7 +87,7 @@ export class BambooSlateDevice {
 
             // Get LIVE service
             return bamboo.server.getPrimaryService(WACOM_LIVE_SERVICE_UUID);
-            
+
         }).then(function (service) {
 
             // Get TX characteristic
@@ -109,17 +115,22 @@ export class BambooSlateDevice {
         });
     }
 
-    exec(opcode, args = [0x00], opt) {
+    /**
+     * Execute an operation on the Bamboo Slate.
+     * @param {number} opcode Operation code.
+     * @param {Array} args Payload array.
+     */
+    exec(opcode, args = [0x00]) {
         var bamboo = this;
-        
+
         // Create packet
         var packet = new Uint8Array([opcode, args.length].concat(args));
-        
+
         // Create promise for response and add event listener
-        var promise = new Promise(function(resolve, reject) {
-            bamboo.rx.addEventListener("characteristicvaluechanged", function(event){
+        var promise = new Promise(function (resolve, reject) {
+            bamboo.rx.addEventListener("characteristicvaluechanged", function (event) {
                 var response = new Uint8Array(event.target.value.buffer);
-                
+
                 resolve({
                     opcode: response[0],
                     args: response.slice(2)
@@ -132,23 +143,19 @@ export class BambooSlateDevice {
         this.tx.writeValue(packet.buffer);
 
         return promise;
-        
+
     }
 
+    /**
+     * Register with the device.
+     */
     register() {
-        // Start
         this.exec(0xE7, this.uuid);
-
-        // Finish
-        // this.exec(0xE5);
-
-        // bambooSlate.exec(0xb6, [169, 124, 249, 91, 0, 0]) // set time
-        // bambooSlate.exec(0xD6) // get time
-        // bambooSlate.exec(0xec, [0x06, 0x00, 0x00, 0x00, 0x00, 0x00]) // ec
-        // bambooSlate.exec(0xdb) // get name
-        // bambooSlate.exec(0xc1) // is data available?
     }
 
+    /**
+     * Turn the device into "live-mode". This initiates the streaming of pen data.
+     */
     startLive() {
         var self = this;
 
@@ -157,10 +164,17 @@ export class BambooSlateDevice {
         });
     }
 
+    /**
+     * Check the connection with the device.
+     * @returns {boolean} Connection is ok.
+     */
     checkConnection() {
         return this.exec(0xe6, [0xF5, 0x4B, 0x32, 0x92, 0xC, 0x88]);
     }
 
+    /**
+     * Break connection with device.
+     */
     disconnect() {
         if (!this.device) {
             return Promise.reject('Device is not connected.');
@@ -168,24 +182,35 @@ export class BambooSlateDevice {
         return this.device.gatt.disconnect();
     }
 
+    /**
+     * Called if the connection is lost. Will attempt to reconnect.
+     */
     onDisconnected() {
         this.connect();
     }
 
+    /**
+     * Called if there is an error during the connection sequence.
+     * @param {Error} err 
+     */
     onError(err) {
         console.error(err);
     }
 
+    /**
+     * Called when pen data is recieved from the device. Event value contains a buffer that must be decoded.
+     * @param {*} event 
+     */
     onPenData(event) {
         var buffer = event.target.value.buffer;
         var dv = new DataView(buffer, 0);
 
-        if(dv.getUint8(0) == 0xa1) {
-            for(var i=2; i+6<dv.byteLength; i+=6) {
+        if (dv.getUint8(0) == 0xa1) {
+            for (var i = 2; i + 6 < dv.byteLength; i += 6) {
                 var z = dv.getInt16(i + 4, true);
                 var point = [dv.getInt16(i, true), dv.getInt16(i + 2, true), z, 0, Math.random()]; // [x, y, pressure, is_new_stroke, random]
-                if(z > 0) {
-                    if(this.lastPoint[2] == 0) point[3] = 1;
+                if (z > 0) {
+                    if (this.lastPoint[2] == 0) point[3] = 1;
                     this.onDraw(point[0], point[1], point[2], point[3]);
                 }
                 this.lastPoint = point;
